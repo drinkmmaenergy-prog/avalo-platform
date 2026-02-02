61d16d1927f647085aac62f4212baa205c01256b
TwojaNazwa <TwjEmail>
2026-02-02 19:02:17 +0100

    chore(repo): remove fix scripts, shared legacy and build artifacts (canonical cleanup)

--- shared/compliance/pack418-compliance-constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CCPA_REQUIREMENTS = exports.GDPR_REQUIREMENTS = exports.COMPLIANCE_REGIONS = void 0;
-// Stub types for pack418 compliance
-exports.COMPLIANCE_REGIONS = ['EU', 'US', 'UK', 'APAC'];
-exports.GDPR_REQUIREMENTS = [];
-exports.CCPA_REQUIREMENTS = [];
--- shared/config/pack416-feature-flags.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getFeatureFlag = exports.FEATURE_FLAGS = void 0;
-exports.FEATURE_FLAGS = {};
-const getFeatureFlag = (flagId) => exports.FEATURE_FLAGS[flagId];
-exports.getFeatureFlag = getFeatureFlag;
