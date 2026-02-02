106cf503e5fa38274a3cd35c935ccc3bec355697
TwojaNazwa <TwjEmail>
2026-02-02 19:02:17 +0100

    chore(repo): remove fix scripts, shared legacy and build artifacts (canonical cleanup)

--- shared/integration/pack414-registry.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.INTEGRATION_REGISTRY = void 0;
-exports.INTEGRATION_REGISTRY = {};
--- shared/legal/legalRegistry.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getCurrentLegalVersion = exports.LEGAL_DOCUMENTS = void 0;
-exports.LEGAL_DOCUMENTS = {};
-const getCurrentLegalVersion = (type) => '1.0.0';
-exports.getCurrentLegalVersion = getCurrentLegalVersion;
